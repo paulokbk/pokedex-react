49de7cd96a9a1003f17ed5e03e49b13f6989f875
Paulo Filho <paulokbkfilho@hotmail.com>
2022-11-08 18:35:58 -0300

    fix: loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export default function Loading () {
     return (
-        <Box className='loading' display='flex' flexDirection='column' justifyContent='center' alignItems='center' gap='40px'>
+        <Box className='loading' display='flex' flexDirection='column' justifyContent='center' alignItems='center' gap='40px' height='80vh'>
         <img className='logo' src="/assets/pokemon.svg" alt="pokemon.svg" />
         <span>CARREGANDO...</span>
         </Box>
